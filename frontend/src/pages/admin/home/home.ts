4daeca6c74ba6416786d89dc5c32f6b523499109
Saul Hillar <saulhillar@outlook.es>
2026-07-02 19:19:29 -0300

    feat: dashboard cards counts

--- frontend/src/pages/admin/home/home.ts
+++ frontend/src/pages/admin/home/home.ts
@@ -1,4 +1,52 @@
+import { CategoriaService } from "../../../services/categoryService";
+import { getOrders } from "../../../services/orderService";
+import { productService } from "../../../services/productService";
 import { checkAuhtUser, logout } from "../../../utils/auth";
+
+const categoriesCount = document.getElementById(
+  "categories-count"
+) as HTMLParagraphElement;
+
+const productsCount = document.getElementById(
+  "products-count"
+) as HTMLParagraphElement;
+
+const ordersCount = document.getElementById(
+  "orders-count"
+) as HTMLParagraphElement;
+
+const productsActiveCount = document.getElementById(
+  "products-active-count"
+) as HTMLParagraphElement;
+
+const summaryText = document.getElementById(
+  "summary-text"
+) as HTMLParagraphElement;
+
+
+async function getCategoriesCount() {
+  const categories = await CategoriaService.getAll();
+  categoriesCount.textContent = categories.length.toString();
+}
+
+async function getProductsCount() {
+  const products = await productService.getAll();
+  productsCount.textContent = products.length.toString();
+}
+
+async function getOrdersCount() {
+  const orders = await getOrders();
+  ordersCount.textContent = orders.length.toString();
+}
+
+async function getProductsActiveCount() {
+  const products = await productService.getAll();
+  productsActiveCount.textContent = products.length.toString();
+}
+
+async function getSummaryText() {
+  summaryText.textContent = "Cargando estadísticas...";
+}
 
 const buttonLogout = document.getElementById(
   "logout-btn"
@@ -15,6 +63,11 @@ const initPage = () => {
     "/src/pages/store/home/home.html",
     "admin"
   );
+  getCategoriesCount();
+  getProductsCount();
+  getOrdersCount();
+  getProductsActiveCount();
+  getSummaryText();
 };
 
 initPage();
